8fe5cc7fe9f531d1a4b4ff283093fd482952ba48
itsPareshY <itspareshyadav@gmail.com>
2021-12-08 14:20:31 +0530

    Remove unused

--- src/main/java/org/example/SpringBootApp/dao/IEmployeeDAO.java
+++
@@ -1,4 +0,0 @@
-package org.example.SpringBootApp.dao;
-
-public interface IEmployeeDAO {
-}
--- src/main/java/org/example/SpringBootApp/daoimpl/EmployeeDAOImpl.java
+++
@@ -1,6 +0,0 @@
-package org.example.SpringBootApp.daoimpl;
-
-import org.example.SpringBootApp.dao.IEmployeeDAO;
-
-public class EmployeeDAOImpl implements IEmployeeDAO {
-}
